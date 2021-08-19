53dfd20d3b18e828bf0ab77c7ea1dc6eb9ed386f
Johnny Goth <Jgoth23@gmail.com>
2021-08-19 11:52:38 -0500

    added dynamic placehlders

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,16 +3,16 @@ import './ExpenseItem.css';
 function ExpenseItem() {
 
 const expenseDate = new Date(2021, 2, 18);
-const expenseTitle = 'Health Insurance';
+const expenseTitle = 'Car Insurance';
 const expenseAmount = 55.85;
 
   return (
     <div className="expense-item">
-      <div>March 18th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Health Insurance </h2>
+        <h2>{expenseTitle}</h2>
       </div>
-      <div className="expense-item__price">55.85</div>
+      <div className="expense-item__price">{expenseAmount}</div>
     </div>
   );
 }
