21da656184078855ed6b8de77baf6977a4f78691
Johnny Goth <Jgoth23@gmail.com>
2021-08-19 11:25:45 -0500

    styles added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,10 @@ function ExpenseItem() {
   return (
     <div className="expense-item">
       <div>Aug 19th 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Health Insurance </h2>
       </div>
-      <div>55.85</div>
+      <div className="expense-item__price">55.85</div>
     </div>
   );
 }
