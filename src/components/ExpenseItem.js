dbb47121dbbc6dc57082c0a5f539fa95f9d1f04e
Johnny Goth <Jgoth23@gmail.com>
2021-08-19 11:41:45 -0500

    added some JS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+const expenseDate = new Date(2021, 2, 18);
+const expenseTitle = 'Health Insurance';
+const expenseAmount = 55.85;
+
   return (
     <div className="expense-item">
-      <div>Aug 19th 2021</div>
+      <div>March 18th 2021</div>
       <div className="expense-item__description">
         <h2>Health Insurance </h2>
       </div>
