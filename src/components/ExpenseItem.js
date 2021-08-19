d4e801adcda61e41cd62a39a834bf2b4074aaa02
Johnny Goth <Jgoth23@gmail.com>
2021-08-19 11:01:44 -0500

    added new code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
 function ExpenseItem() {
   return (
     <div>
-      <h2>New comp</h2>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>Amount</div>
     </div>
-  )
+  );
 }
 
 export default ExpenseItem;
