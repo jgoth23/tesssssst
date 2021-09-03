4272bea515453251856f1a7edb787388e74315ba
Johnny Goth <Jgoth23@gmail.com>
2021-09-03 12:44:52 -0500

    added button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,8 +8,9 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-      </div>
       <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button>Change Title</button>
     </Card>
   );
 }
