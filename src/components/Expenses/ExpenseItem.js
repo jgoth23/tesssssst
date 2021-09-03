4ab229b2be8be9ef5cd7922ccbd4a4de0c0cb49c
Johnny Goth <Jgoth23@gmail.com>
2021-09-03 12:53:00 -0500

    console click is working

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
       <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={() =>{console.log('Clicked!!')}}>Change Title</button>
     </Card>
   );
 }
